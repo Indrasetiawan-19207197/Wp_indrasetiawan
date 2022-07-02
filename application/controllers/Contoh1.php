30ee18fd1b8af588dff8f2a2d1e484dbe6ba7580
indrasetiawan-19207197 <sugendret806@gmail.com>
2022-07-02 10:14:40 +0700

    menambahkan file tugas

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama saya Indra setiawan beralamat dim mampang prapatan jakarta selatan, Saya suka bermain alat musik";
+	}
+}
